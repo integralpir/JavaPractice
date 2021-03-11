b2405baceb16f6560a2888a95fbed261d1c9cb2b
integralpir <roma_5712@mail.ru>
2021-03-11 20:04:36 +0500

    new

---
+++ Example2.java
@@ -0,0 +1,12 @@
+class Example2 {
+    public static void main(String args[]){
+        int x, y;
+        y = 10;
+        for (x = 0; x <= 10; x++) {
+            System.out.print("X value: ");
+            System.out.println(x);
+            System.out.println("Y value: " + y);
+            y -= 1;
+        }
+    }
+}
---
+++ Example3.java
@@ -0,0 +1,7 @@
+class Example3 {
+    public static void main(String args[]) {
+        int x;
+        for (x = 0; x < 10; x++)
+            System.out.println("X value: " + x);
+    }
+}
