9823f2d9d76edb90e6557a02a00c69c42a763d4d
integralpir <roma_5712@mail.ru>
2021-03-14 20:38:53 +0500

    14.03.21

---
+++ Tasks/Stepic2_5_4.java
@@ -0,0 +1,36 @@
+/*
+Решение задачи со степика
+курс алгоритмизация и программирование
+
+п.с. массивы удобнее инициализировать на джаве
+ */
+
+class Stepic2_5_4 {
+    public static void main(String args[]){
+        int n = 3;
+        int m = 4;
+        int i = 1;
+        int array[][] = new int[n][m];
+
+        for (int j = 0; j < m; j++){
+            if (j % 2 == 0){
+                for (int k = 0; k < n; k++){
+                    array[k][j] = i;
+                    i++;
+                }
+            } else {
+                for (int k = (n - 1); k >= 0; k -= 1){
+                    array[k][j] = i;
+                    i++;
+                }
+            }
+        }
+
+        for (int j = 0; j < n; j++){
+            for (int k = 0; k < m; k++){
+                System.out.print(array[j][k] + " ");
+            }
+        System.out.println();
+        }
+    }
+}
