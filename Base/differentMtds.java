b1feb43313dd7947d1b6a3d8542a26e569d3a785
integralpir <roma_5712@mail.ru>
2021-03-15 00:12:16 +0500

    15.03.21

---
+++ Base/differentMtds.java
@@ -0,0 +1,16 @@
+class differentMtds {
+    public static void main(String args[]){
+        int x = 2;
+        int y = 3;
+        System.out.println(sum(x, y));
+        System.out.println(mlt(x, y));
+    }
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    public static int mlt(int a, int b){
+        return a * b;
+    }
+}
