1be1fdd10e516591f484b746b92170a90802b762
integralpir <roma_5712@mail.ru>
2021-04-15 19:49:10 +0500

    15.04.21

--- Tasks/LabRab_4.java
+++ Tasks/LabRab_4.java
@@ -1,10 +1,7 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
 
-import static java.nio.file.Paths.*;
 
 public class LabRab_4 {
     public static Scanner scanner = new Scanner(System.in);
@@ -19,6 +16,8 @@ public class LabRab_4 {
             case(3) -> Task_3();
             case(4) -> Task_4();
             case(5) -> Task_5();
+            case(6) -> Task_6();
+            case(7) -> Task_7();
         }
     }
 
@@ -57,28 +56,91 @@ public class LabRab_4 {
     }
 
     public static void Task_4(){
-        try (var reader = new FileReader(src);
-             var buffer = new BufferedReader(reader)) {
+        try(var reader = new FileReader(src);
+            var buffer = new BufferedReader(reader)) {
             int sum = 0;
-            while (reader.ready()){
-                var list = new ArrayList<String[]>();
-                while (buffer.ready()){
-                    String str = buffer.readLine();
-                    list.add(str.split(" "));
-                }
-                for (String[] tab : list) {
-                    for (String s : tab) {
-                        sum += Integer.parseInt(s);
-                    }
-                }
-                System.out.println(sum);
+            var list = new ArrayList<String[]>();
+            while (buffer.ready()){
+                String str = buffer.readLine();
+                list.add(str.split(" "));
             }
+            for (String[] tab : list) {
+                for (String s : tab) {
+                    sum += Integer.parseInt(s);
+                }
+            }
+            System.out.println(sum);
         } catch (IOException e) {
             System.out.println("Something got wrong");
         }
     }
 
     public static void Task_5(){
+        try(var reader = new FileReader(src);
+            var buffer = new BufferedReader(reader)){
+            var summary = new ArrayList<Integer>();
+            var list = new ArrayList<String[]>();
+            while (buffer.ready()){
+                String str = buffer.readLine();
+                list.add(str.split(" "));
+            }
+            for (String[] tab : list) {
+                int sum = 0;
+                for (String s : tab) {
+                    sum += Integer.parseInt(s);
+                }
+                summary.add(sum);
+            }
+            int index = summary.indexOf(Collections.max(summary));
+            System.out.println(Arrays.toString(list.get(index)));
+        } catch (IOException e){
+            System.out.println("Something got wrong");
+        }
+    }
 
+    public static void Task_6(){
+        int mainSum = 0;
+        int addSum = 0;
+        try(var reader = new FileReader(src);
+            var buffer = new BufferedReader(reader))
+        {
+            var list = new ArrayList<String[]>();
+
+            while (buffer.ready()){
+                String str = buffer.readLine();
+                list.add(str.split(" "));
+            }
+
+            for (int i = 0; i < list.size(); i++){
+                mainSum += Integer.parseInt(list.get(i)[i]);
+                addSum += Integer.parseInt(list.get(i)[list.size() - i - 1]);
+            }
+
+            System.out.printf("%d %d", mainSum, addSum);
+        } catch (IOException e){
+            System.out.println("Something got wrong");
+        }
+    }
+
+    public static void Task_7(){
+        try(var reader = new FileReader(src);
+            var buffer = new BufferedReader(reader))
+        {
+            var list = new ArrayList<String[]>();
+            while (buffer.ready()){
+                String str = buffer.readLine();
+                list.add(str.split(" "));
+            }
+
+            String[][] tabs = new String[list.size()][list.get(0).length];
+            for (int i = 0; i < tabs.length; i++){
+                for (int j = 0; j < tabs[i].length; j++){
+                    tabs[i][j] = list.get(j)[i];
+                }
+                System.out.println(Arrays.toString(tabs[i]));
+            }
+        } catch (IOException e){
+            System.out.println("Something got wrong.");
+        }
     }
 }
